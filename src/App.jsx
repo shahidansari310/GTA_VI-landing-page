b35024d7d9cb39bda75e5f64c8b4b8c3636a04e5
shahidansari310 <shahidansaribhodu@gmail.com>
2025-11-25 21:48:36 +0530

    completed landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from "gsap"
+import 'remixicon/fonts/remixicon.css'
+
 
 const App = () => {
 
@@ -76,11 +78,26 @@ const App = () => {
               <h3 className='text-2xl text-white font-bold mt-[-6px] leading-none'>Rockstar</h3>
              </div>
           </div>
+          
           <div className="imagesdiv w-full h-screen relative overflow-hidden">
             <img src=".\sky.png" alt="sky-bg" className='absolute top-0 left-0 w-full h-full object-cover' />
             <img src=".\bg (1).png" alt="background" className='absolute top-0 left-0 w-full h-full object-cover'/>
-            <img src="public\girlbg.png" alt="background" className=' character h-[400px]
+
+            <div className="text absolute top-0 left-1/2 -translate-x-1/2 flex flex-col gap-2 mt-3">
+            <h1 className='text-white text-9xl -ml-18 leading-none'>grand</h1>
+            <h1 className='text-white text-9xl ml-18 leading-none'>theft</h1>
+            <h1 className='text-white text-9xl -ml-18 leading-none'>auto</h1>
+          </div>
+            <img src="/girlbg.png" alt="background" className=' character h-[400px]
             absolute -bottom-[25%] left-1/2 -translate-x-1/2 scale-[1.4]'/>
+            
+          </div>
+          <div className="btmbar text-white absolute bottom-0 left-0 w-full py-8 px-8 bg-gradient-to-t from-black to-transparent">
+            <div className="flex gap-1 items-center">
+              <i className='ri-arrow-down-line text-[15px]'></i>
+              <h3 className="font-sans text-[12px]">Scroll Down</h3>
+            </div>
+            <img className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[40px]' src="./ps5.png" alt="" />
           </div>
         </div>
       </div>
